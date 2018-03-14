97edcbb506da894b1973d94a6a346ea3ef8ba479
Matthew Montgomery <matthew@signed8bit.com>
2018-03-14 14:59:14 -0500

    Reformats all Java source using google-java-format
    Change-Id: Ie62fc3050a59e4da1868b4614b1074dc88688a73

--- src/main/java/com/cisco/gerrit/plugins/slack/config/ProjectConfig.java
+++ src/main/java/com/cisco/gerrit/plugins/slack/config/ProjectConfig.java
@@ -28,189 +28,166 @@ import org.slf4j.LoggerFactory;
  *
  * @author Matthew Montgomery
  */
-public class ProjectConfig
-{
-    /**
-     * The class logger instance.
-     */
-    private static final Logger LOGGER = LoggerFactory.getLogger(
-            ProjectConfig.class);
+public class ProjectConfig {
+  /** The name of the plugin config section to lookup within the gerrit.config file. */
+  public static final String CONFIG_NAME = "slack-integration";
+  /** The class logger instance. */
+  private static final Logger LOGGER = LoggerFactory.getLogger(ProjectConfig.class);
+  private boolean enabled;
+  private String webhookUrl;
+  private String channel;
+  private String username;
+  private String ignore;
+  private boolean ignoreUnchangedPatchSet;
+  private boolean ignoreWorkInProgressPatchSet;
+  private boolean ignorePrivatePatchSet;
+  private boolean publishOnPatchSetCreated;
+  private boolean publishOnChangeMerged;
+  private boolean publishOnCommentAdded;
+  private boolean publishOnReviewerAdded;
+  private boolean publishOnWipReady;
+  private boolean publishOnPrivateToPublic;
 
-    /**
-     * The name of the plugin config section to lookup within the gerrit.config
-     * file.
-     */
-    public static final String CONFIG_NAME = "slack-integration";
+  /**
+   * Creates a new instance of the ProjectConfig class for the given project.
+   *
+   * @param configFactory The Gerrit PluginConfigFactory instance to use.
+   * @param project The project to use when looking up a configuration.
+   */
+  public ProjectConfig(PluginConfigFactory configFactory, String project) {
+    enabled = false;
 
-    private boolean enabled;
-    private String webhookUrl;
-    private String channel;
-    private String username;
-    private String ignore;
-    private boolean ignoreUnchangedPatchSet;
-    private boolean ignoreWorkInProgressPatchSet;
-    private boolean ignorePrivatePatchSet;
-    private boolean publishOnPatchSetCreated;
-    private boolean publishOnChangeMerged;
-    private boolean publishOnCommentAdded;
-    private boolean publishOnReviewerAdded;
-    private boolean publishOnWipReady;
-    private boolean publishOnPrivateToPublic;
+    Project.NameKey projectNameKey;
+    projectNameKey = Project.NameKey.parse(project);
 
-    /**
-     * Creates a new instance of the ProjectConfig class for the given project.
-     *
-     * @param configFactory The Gerrit PluginConfigFactory instance to use.
-     * @param project The project to use when looking up a configuration.
-     */
-    public ProjectConfig(PluginConfigFactory configFactory, String project)
-    {
-        enabled = false;
+    try {
+      enabled =
+          configFactory
+              .getFromProjectConfigWithInheritance(projectNameKey, CONFIG_NAME)
+              .getBoolean("enabled", false);
 
-        Project.NameKey projectNameKey;
-        projectNameKey = Project.NameKey.parse(project);
+      webhookUrl =
+          configFactory
+              .getFromProjectConfigWithInheritance(projectNameKey, CONFIG_NAME)
+              .getString("webhookurl", "");
 
-        try
-        {
-            enabled = configFactory.getFromProjectConfigWithInheritance(
-                    projectNameKey, CONFIG_NAME).getBoolean(
-                    "enabled", false);
+      channel =
+          configFactory
+              .getFromProjectConfigWithInheritance(projectNameKey, CONFIG_NAME)
+              .getString("channel", "general");
 
-            webhookUrl = configFactory.getFromProjectConfigWithInheritance(
-                    projectNameKey, CONFIG_NAME).getString(
-                    "webhookurl", "");
+      username =
+          configFactory
+              .getFromProjectConfigWithInheritance(projectNameKey, CONFIG_NAME)
+              .getString("username", "gerrit");
 
-            channel = configFactory.getFromProjectConfigWithInheritance(
-                    projectNameKey, CONFIG_NAME).getString(
-                    "channel", "general");
+      ignore =
+          configFactory
+              .getFromProjectConfigWithInheritance(projectNameKey, CONFIG_NAME)
+              .getString("ignore", "");
 
-            username = configFactory.getFromProjectConfigWithInheritance(
-                    projectNameKey, CONFIG_NAME).getString(
-                    "username", "gerrit");
+      ignoreUnchangedPatchSet =
+          configFactory
+              .getFromProjectConfigWithInheritance(projectNameKey, CONFIG_NAME)
+              .getBoolean("ignore-unchanged-patch-set", true);
 
-            ignore = configFactory.getFromProjectConfigWithInheritance(
-                    projectNameKey, CONFIG_NAME).getString(
-                    "ignore", "");
+      ignoreWorkInProgressPatchSet =
+          configFactory
+              .getFromProjectConfigWithInheritance(projectNameKey, CONFIG_NAME)
+              .getBoolean("ignore-wip-patch-set", true);
 
-            ignoreUnchangedPatchSet =
-                configFactory.getFromProjectConfigWithInheritance(
-                    projectNameKey, CONFIG_NAME).getBoolean(
-                    "ignore-unchanged-patch-set", true);
+      ignorePrivatePatchSet =
+          configFactory
+              .getFromProjectConfigWithInheritance(projectNameKey, CONFIG_NAME)
+              .getBoolean("ignore-private-patch-set", true);
 
-            ignoreWorkInProgressPatchSet =
-                configFactory.getFromProjectConfigWithInheritance(
-                    projectNameKey, CONFIG_NAME).getBoolean(
-                    "ignore-wip-patch-set", true);
+      publishOnPatchSetCreated =
+          configFactory
+              .getFromProjectConfigWithInheritance(projectNameKey, CONFIG_NAME)
+              .getBoolean("publish-on-patch-set-created", true);
 
-            ignorePrivatePatchSet =
-                configFactory.getFromProjectConfigWithInheritance(
-                    projectNameKey, CONFIG_NAME).getBoolean(
-                    "ignore-private-patch-set", true);
+      publishOnChangeMerged =
+          configFactory
+              .getFromProjectConfigWithInheritance(projectNameKey, CONFIG_NAME)
+              .getBoolean("publish-on-change-merged", true);
 
-            publishOnPatchSetCreated =
-                configFactory.getFromProjectConfigWithInheritance(
-                    projectNameKey, CONFIG_NAME).getBoolean(
-                    "publish-on-patch-set-created", true);
+      publishOnCommentAdded =
+          configFactory
+              .getFromProjectConfigWithInheritance(projectNameKey, CONFIG_NAME)
+              .getBoolean("publish-on-comment-added", true);
 
-            publishOnChangeMerged =
-                configFactory.getFromProjectConfigWithInheritance(
-                    projectNameKey, CONFIG_NAME).getBoolean(
-                    "publish-on-change-merged", true);
+      publishOnReviewerAdded =
+          configFactory
+              .getFromProjectConfigWithInheritance(projectNameKey, CONFIG_NAME)
+              .getBoolean("publish-on-reviewer-added", true);
 
-            publishOnCommentAdded =
-                configFactory.getFromProjectConfigWithInheritance(
-                    projectNameKey, CONFIG_NAME).getBoolean(
-                    "publish-on-comment-added", true);
+      publishOnWipReady =
+          configFactory
+              .getFromProjectConfigWithInheritance(projectNameKey, CONFIG_NAME)
+              .getBoolean("publish-on-wip-ready", publishOnPatchSetCreated);
 
-            publishOnReviewerAdded =
-                configFactory.getFromProjectConfigWithInheritance(
-                    projectNameKey, CONFIG_NAME).getBoolean(
-                    "publish-on-reviewer-added", true);
-
-            publishOnWipReady =
-                configFactory.getFromProjectConfigWithInheritance(
-                    projectNameKey, CONFIG_NAME).getBoolean(
-                    "publish-on-wip-ready", publishOnPatchSetCreated);
-
-            publishOnPrivateToPublic =
-                configFactory.getFromProjectConfigWithInheritance(
-                    projectNameKey, CONFIG_NAME).getBoolean(
-                    "publish-on-private-to-public", publishOnPatchSetCreated);
-        }
-        catch (NoSuchProjectException e)
-        {
-            LOGGER.warn("The specified project could not be found: " +
-                    project);
-        }
+      publishOnPrivateToPublic =
+          configFactory
+              .getFromProjectConfigWithInheritance(projectNameKey, CONFIG_NAME)
+              .getBoolean("publish-on-private-to-public", publishOnPatchSetCreated);
+    } catch (NoSuchProjectException e) {
+      LOGGER.warn("The specified project could not be found: " + project);
     }
+  }
 
-    public boolean isEnabled()
-    {
-        return enabled;
-    }
+  public boolean isEnabled() {
+    return enabled;
+  }
 
-    public String getWebhookUrl()
-    {
-        return webhookUrl;
-    }
+  public String getWebhookUrl() {
+    return webhookUrl;
+  }
 
-    public String getChannel()
-    {
-        return channel;
-    }
+  public String getChannel() {
+    return channel;
+  }
 
-    public String getUsername()
-    {
-        return username;
-    }
+  public String getUsername() {
+    return username;
+  }
 
-    public String getIgnore()
-    {
-        return ignore;
-    }
+  public String getIgnore() {
+    return ignore;
+  }
 
-    public boolean getIgnoreUnchangedPatchSet()
-    {
-        return ignoreUnchangedPatchSet;
-    }
+  public boolean getIgnoreUnchangedPatchSet() {
+    return ignoreUnchangedPatchSet;
+  }
 
-    public boolean getIgnoreWorkInProgressPatchSet()
-    {
-        return ignoreWorkInProgressPatchSet;
-    }
+  public boolean getIgnoreWorkInProgressPatchSet() {
+    return ignoreWorkInProgressPatchSet;
+  }
 
-    public boolean getIgnorePrivatePatchSet()
-    {
-        return ignorePrivatePatchSet;
-    }
+  public boolean getIgnorePrivatePatchSet() {
+    return ignorePrivatePatchSet;
+  }
 
-    public boolean shouldPublishOnPatchSetCreated()
-    {
-        return publishOnPatchSetCreated;
-    }
+  public boolean shouldPublishOnPatchSetCreated() {
+    return publishOnPatchSetCreated;
+  }
 
-    public boolean shouldPublishOnChangeMerged()
-    {
-        return publishOnChangeMerged;
-    }
+  public boolean shouldPublishOnChangeMerged() {
+    return publishOnChangeMerged;
+  }
 
-    public boolean shouldPublishOnCommentAdded()
-    {
-        return publishOnCommentAdded;
-    }
+  public boolean shouldPublishOnCommentAdded() {
+    return publishOnCommentAdded;
+  }
 
-    public boolean shouldPublishOnReviewerAdded()
-    {
-        return publishOnReviewerAdded;
-    }
+  public boolean shouldPublishOnReviewerAdded() {
+    return publishOnReviewerAdded;
+  }
 
-    public boolean shouldPublishOnWipReady()
-    {
-        return publishOnWipReady;
-    }
+  public boolean shouldPublishOnWipReady() {
+    return publishOnWipReady;
+  }
 
-    public boolean shouldPublishOnPrivateToPublic()
-    {
-        return publishOnPrivateToPublic;
-    }
+  public boolean shouldPublishOnPrivateToPublic() {
+    return publishOnPrivateToPublic;
+  }
 }
